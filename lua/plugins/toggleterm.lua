12f87e0473a1ce1a915a18d0b0771c810385e765
ElyWacime <yousfi.wacime@gmail.com>
2025-03-31 19:05:48 +0200

    update

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,17 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    size = 10,
+    open_mapping = [[<C-\>]],
+    shading_factor = 2,
+    direction = "float",
+    float_opts = {
+      border = "curved",
+      highlights = {
+        border = "Normal",
+        background = "Normal",
+      },
+    },
+  },
+}
