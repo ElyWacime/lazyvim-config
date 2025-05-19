f816d2b171cde6c2cda05d1f680c0a93e2550c20
elywacime <yousfi.wacime@gmail.com>
2025-05-19 14:16:16 +0000

    latest

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,34 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      clangd = {}, -- C/C++
-      pyright = {}, -- Python
-      tsserver = {}, -- JavaScript/TypeScript
-      lua_ls = {}, -- Lua
-    },
-  },
+	"neovim/nvim-lspconfig",
+	dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
+	config = function()
+		-- Ensure that Mason and Mason-LSPconfig are set up
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "clangd", "pyright", "lua_ls" }, -- List your LSP servers here
+		})
+
+		-- Set up LSP servers using lspconfig
+		local lspconfig = require("lspconfig")
+
+		-- Configuring clangd (C/C++)
+		lspconfig.clangd.setup({
+			-- You can add additional settings here
+		})
+
+		-- Configuring pyright (Python)
+		lspconfig.pyright.setup({
+			-- You can add additional settings here
+		})
+
+		-- Configuring tsserver (JavaScript/TypeScript)
+		lspconfig.tsserver.setup({
+			-- You can add additional settings here
+		})
+
+		-- Configuring lua_ls (Lua)
+		lspconfig.lua_ls.setup({
+			-- You can add additional settings here
+		})
+	end,
 }
