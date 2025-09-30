81575d02668ae404e818b1c2125dfa2028c3d333
elywacime <yousfi.wacime@gmail.com>
2025-09-30 05:09:35 +0100

    upadte

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,71 @@
 
 vim.opt.relativenumber = true -- Enable relative line numbers
 vim.opt.number = true -- Show the absolute number on the current line
+
+-- Force filetype detection for common languages
+vim.filetype.add({
+	extension = {
+		-- scripting
+		sh = "sh",
+		bash = "bash",
+		py = "python",
+		rb = "ruby",
+		pl = "perl",
+		php = "php",
+
+		-- web / frontend
+		html = "html",
+		htm = "html",
+		css = "css",
+		js = "javascript",
+		cjs = "javascript",
+		mjs = "javascript",
+		ts = "typescript",
+		tsx = "tsx",
+		jsx = "javascriptreact",
+		vue = "vue",
+		svelte = "svelte",
+
+		-- data / config
+		json = "json",
+		jsonc = "jsonc",
+		yaml = "yaml",
+		yml = "yaml",
+		toml = "toml",
+		xml = "xml",
+		csv = "csv",
+		ini = "dosini",
+		conf = "conf",
+
+		-- markdown / docs
+		md = "markdown",
+		markdown = "markdown",
+		rst = "rst",
+
+		-- lua / vim
+		lua = "lua",
+		vim = "vim",
+
+		-- systems / compiled
+		c = "c",
+		h = "c",
+		cpp = "cpp",
+		cxx = "cpp",
+		cc = "cpp",
+		hpp = "cpp",
+		java = "java",
+		kt = "kotlin",
+		rs = "rust",
+		go = "go",
+		swift = "swift",
+		scala = "scala",
+		cs = "cs",
+
+		-- docker / infra
+		Dockerfile = "dockerfile",
+		tf = "terraform",
+	},
+})
+
+vim.treesitter.language.register("tsx", "typescriptreact")
+vim.treesitter.language.register("javascript", "javascriptreact")
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,4 @@
+
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
@@ -14,4 +15,71 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("toggleterm").setup(opts)
+
+    -- Terminal helper
+    local Terminal = require("toggleterm.terminal").Terminal
+    local terminals = {}
+    local index = 1
+
+    -- New terminal
+    vim.keymap.set("n", "<leader>tn", function()
+      local term = Terminal:new()
+      table.insert(terminals, term)
+      index = #terminals
+      term:toggle()
+    end, { desc = "New Terminal" })
+
+    -- Next terminal
+    vim.keymap.set("n", "<leader>tv", function()
+      if #terminals == 0 then return end
+      index = index % #terminals + 1
+      terminals[index]:toggle()
+    end, { desc = "Next Terminal" })
+
+    -- Previous terminal
+    vim.keymap.set("n", "<leader>tc", function()
+      if #terminals == 0 then return end
+      index = (index - 2) % #terminals + 1
+      terminals[index]:toggle()
+    end, { desc = "Previous Terminal" })
+
+    -- Close current terminal (Normal mode)
+    vim.keymap.set("n", "<leader>tx", function()
+      if #terminals == 0 then return end
+      if terminals[index] then
+        terminals[index]:shutdown()  -- ✅ kill process + buffer
+        table.remove(terminals, index)
+        if index > #terminals then
+          index = #terminals
+        end
+      end
+    end, { desc = "Close Current Terminal" })
+
+    -- Close current terminal (Terminal mode)
+    vim.keymap.set("t", "<leader>tx", function()
+      vim.api.nvim_feedkeys(
+        vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true),
+        "n",
+        true
+      )
+      vim.schedule(function()
+        if #terminals == 0 then return end
+        if terminals[index] then
+          terminals[index]:shutdown()  -- ✅ kill process + buffer
+          table.remove(terminals, index)
+          if index > #terminals then
+            index = #terminals
+          end
+        end
+      end)
+    end, { desc = "Close Current Terminal" })
+
+    -- Escape to Normal mode inside terminal
+    vim.cmd([[
+      tnoremap <Esc> <C-\><C-n>
+    ]])
+  end,
 }
+
